30c4d17b821a7dca799491b2a7921faf0c505296
Tarun Gupta <soul.tarun@gmail.com>
2017-01-13 17:20:43 +0530

    Testing deployment

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ app.use('/maillc',mailcheck);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  var err = new Error('Not Found');
+  var err = new Error('Not Found - Tarun');
   err.status = 404;
   next(err);
 });
